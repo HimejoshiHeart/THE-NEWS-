86d5040959b86f98f7ebd7fa4cbb8e0b8e81b9a9
HimejoshiHeart <nevertobeneverwithme@gmail.com>
2024-11-23 16:20:12 -0500

    javavaavavavva

---
+++ i3.js
@@ -0,0 +1,2 @@
+document.getElementById("pain").innerHTML =
+  ' Upon reporters asking them if they were ok, Siffrin was quoted as saying"...Yes. Very." in an Obviously Not Ok Way, as if they had repeated today and tomorrow about '+ (Math.floor(Math.random()*100) + 100 + (Math.floor(Math.random()*100)*.01)) + ' times.';
