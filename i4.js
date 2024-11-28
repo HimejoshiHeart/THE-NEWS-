68203c5d05a1daa3b5aa165c7c4a354528618785
HimejoshiHeart <nevertobeneverwithme@gmail.com>
2024-11-28 15:26:07 -0500

    yeah

--- i4.js
+++ i4.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){});
-// var hehe = prompt("whats your most embarassing secret")
+var hehe = prompt("whats your most embarassing secret")
 $("#love").click(function(){
     $("#alpha").text("THEY'RE REPEATING THE CRABBING ALPHABET AGAIN")
 $("#idea").html('<p>"'+"I should just crabbing dropkick them. Like, right crabbing <em>now</em>" + '" Bonnie said, clearly miffed.')
@@ -7,4 +7,4 @@ $("#headline > h2").html('<h2>"'+"I should just crabbing dropkick them. Like, ri
 $("#DROPKICK").text("Bonnie then ran over to and dropkicked these two idiots, before running back to the Head Housemaiden like nothing had ever happened")
 $("#wait").text("(In other news, has anyone seen Siffrin's hat?)")
 });
-// $("h4 #h").text = "FURRY'S MOST EMBARASSING SECRET REVEALED! " + '"' + hehe + '"')
+$("#lol").text("FURRY'S MOST EMBARASSING SECRET REVEALED! " + '"' + hehe + '"')
